84d7baea432980342fc394f4947a3efe4e88596c
SeanDuffie <duffiesean@gmail.com>
2024-08-26 14:51:26 -0400

    Added initial documentation

---
+++ clashstats.py
@@ -0,0 +1,31 @@
+""" @file clashstats.py
+    @author Sean Duffie
+    @brief Tracks weekly clan stats
+
+    Every monday various stats should be collected on members of a clan.
+
+    Monthly:
+        - Clan Games
+            - Average Score
+            - Total Score
+        - League Wars
+            - Score
+            - Attacks Used
+            - Included
+    Weekly:
+        - Raids
+            - Average Score
+            - Total Score
+        - Wars
+            - Score
+            - Attacks Used
+            - Included
+        - Roster
+            - Who is in?
+            - What rank are they?
+            - Trophy Count
+            - TH Level
+            - Hero Levels
+"""
+
+
