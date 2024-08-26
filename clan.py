64e4d846a67ffeab9fecc7be297fc1adaa57b8c7
SeanDuffie <duffiesean@gmail.com>
2024-08-26 17:04:35 -0400

    Added more code infrastructure

---
+++ clan.py
@@ -0,0 +1,8 @@
+""" @file clan.py
+    @author Sean Duffie
+    @brief Objectifies the clan
+"""
+
+class Clan:
+    def __init__(self, name: str):
+        
--- clashstats.py
+++ clashstats.py
@@ -5,10 +5,10 @@
     Every monday various stats should be collected on members of a clan.
 
     Monthly:
-        - Clan Games
+        - Clan Games (22nd - 28th)
             - Average Score
             - Total Score
-        - League Wars
+        - League Wars (1st - 8th)
             - Score
             - Attacks Used
             - Included
