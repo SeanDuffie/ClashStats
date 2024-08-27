a54794c759514af5896d4583d6000c5258a3024d
SeanDuffie <duffiesean@gmail.com>
2024-08-27 14:07:51 -0400

    Added several examples

---
+++ examples/discordbot.py
@@ -0,0 +1,261 @@
+# this example assumes you have discord.py > v2.0
+# installed via `python -m pip install -U discord.py`
+# for more info on using discord.py, see the docs at:
+# https://discordpy.readthedocs.io/en/latest
+import asyncio
+import logging
+import os
+import traceback
+
+import discord
+from discord.ext import commands
+
+import coc
+from coc import utils
+
+INFO_CHANNEL_ID = 761848043242127370  # some discord channel ID
+clan_tags = ["#20090C9PR", "#202GG92Q", "#20C8G0RPL"]
+
+############ GET ENVIRONMENT VARIABLES ############
+from dotenv import load_dotenv
+RTDIR = os.path.dirname(__file__)
+
+load_dotenv(dotenv_path=f"{RTDIR}/.env")
+DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
+###################################################
+
+bot = commands.Bot(command_prefix="?", intents=discord.Intents.all())
+
+
+@bot.event
+async def on_ready():
+    print("Logged in!!")
+
+
+@coc.ClanEvents.member_join(tags=clan_tags)
+async def on_clan_member_join(member, clan):
+    await bot.get_channel(INFO_CHANNEL_ID).send(
+        "{0.name} ({0.tag}) just " "joined our clan {1.name} ({1.tag})!".format(
+            member, clan)
+    )
+
+
+@coc.ClanEvents.member_name(tags=clan_tags)
+async def member_name_change(old_player, new_player):
+    await bot.get_channel(INFO_CHANNEL_ID).send(
+        "Name Change! {0.name} is now called {1.name} (his tag is {1.tag})".format(
+            old_player, new_player)
+    )
+
+
+@coc.ClientEvents.event_error()
+async def on_event_error(exception):
+    if isinstance(exception, coc.PrivateWarLog):
+        return  # lets ignore private war log errors
+    print(
+        "Uh oh! Something went wrong in coc.py events... printing traceback for you.")
+    traceback.print_exc()
+
+
+@bot.command()
+async def player_heroes(ctx, player_tag):
+    if not utils.is_valid_tag(player_tag):
+        await ctx.send("You didn't give me a proper tag!")
+        return
+
+    try:
+        player = await ctx.bot.coc_client.get_player(player_tag)
+    except coc.NotFound:
+        await ctx.send("This player doesn't exist!")
+        return
+
+    to_send = ""
+    for hero in player.heroes:
+        to_send += "{}: Lv{}/{}\n".format(str(hero),
+                                          hero.level, hero.max_level)
+
+    await ctx.send(to_send)
+
+
+@bot.command()
+async def parse_army(ctx, army_link: str):
+    troops, spells = ctx.bot.coc_client.parse_army_link(army_link)
+    print(troops, spells)
+    parsed_link_output = ''
+    if troops or spells:  # checking if troops or spells is present in link
+
+        for troop, quantity in troops:
+            parsed_link_output += "The user wants {} {}s. They each have {} DPS.\n".format(
+                quantity, troop.name, troop.dps)
+
+        for spell, quantity in spells:
+            parsed_link_output += "The user wants {} {}s.\n".format(
+                quantity, spell.name)
+    else:
+        parsed_link_output += "Invalid Link!"
+    await ctx.send(parsed_link_output)
+
+
+@bot.command()
+async def create_army(ctx):
+    link = ctx.bot.coc_client.create_army_link(
+        barbarian=10,
+        archer=20,
+        hog_rider=30,
+        healing_spell=3,
+        poison_spell=2,
+        rage_spell=2
+    )
+    await ctx.send(link)
+
+
+@bot.command()
+async def member_stat(ctx, player_tag):
+    if not utils.is_valid_tag(player_tag):
+        await ctx.send("You didn't give me a proper tag!")
+        return
+
+    try:
+        player = await ctx.bot.coc_client.get_player(player_tag)
+    except coc.NotFound:
+        await ctx.send("This clan doesn't exist!")
+        return
+
+    frame = ''
+    if player.town_hall > 11:
+        frame += f"`{'TH Weapon LvL:':<15}` `{player.town_hall_weapon:<15}`\n"
+    role = player.role if player.role else 'None'
+    clan = player.clan.name if player.clan else 'None'
+    frame += (
+        f"`{'Role:':<15}` `{role:<15}`\n"
+        f"`{'Player Tag:':<15}` `{player.tag:<15}`\n"
+        f"`{'Current Clan:':<15}` `{clan:<15.15}`\n"
+        f"`{'League:':<15}` `{player.league.name:<15.15}`\n"
+        f"`{'Trophies:':<15}` `{player.trophies:<15}`\n"
+        f"`{'Best Trophies:':<15}` `{player.best_trophies:<15}`\n"
+        f"`{'War Stars:':<15}` `{player.war_stars:<15}`\n"
+        f"`{'Attack Wins:':<15}` `{player.attack_wins:<15}`\n"
+        f"`{'Defense Wins:':<15}` `{player.defense_wins:<15}`\n"
+        f"`{'Capital Contrib':<15}` `{player.clan_capital_contributions:<15}`\n"
+    )
+    e = discord.Embed(colour=discord.Colour.green(),
+                      description=frame)
+    e.set_thumbnail(url=player.clan.badge.url)
+    await ctx.send(embed=e)
+
+
+@bot.command()
+async def clan_info(ctx, clan_tag):
+
+    if not utils.is_valid_tag(clan_tag):
+        await ctx.send("You didn't give me a proper tag!")
+        return
+
+    try:
+        clan: coc.Clan = await ctx.bot.coc_client.get_clan(clan_tag)
+    except coc.NotFound:
+        await ctx.send("This clan doesn't exist!")
+        return
+
+    if clan.public_war_log is False:
+        log = "Private"
+    else:
+        log = "Public"
+
+    e = discord.Embed(colour=discord.Colour.green())
+
+    e.set_thumbnail(url=clan.badge.url)
+    e.add_field(name="Clan Name",
+                value=f"{clan.name}({clan.tag})\n[Open in game]({clan.share_link})",
+                inline=False)
+    e.add_field(name="Clan Level", value=clan.level, inline=False)
+    e.add_field(name="Description", value=clan.description, inline=False)
+    for district in clan.capital_districts:
+        
+        frame += (f"`{f'{district.name}:':<20}` `{district.hall_level:<15}`\n")
+
+    e2 = discord.Embed(colour=discord.Colour.green(), description=frame,
+                       title="Capital Distracts")
+
+    await ctx.send(embeds=[e, e2])
+
+
+@bot.command()
+async def clan_member(ctx, clan_tag):
+    if not utils.is_valid_tag(clan_tag):
+        await ctx.send("You didn't give me a proper tag!")
+        return
+
+    try:
+        clan = await ctx.bot.coc_client.get_clan(clan_tag)
+    except coc.NotFound:
+        await ctx.send("This clan does not exist!")
+        return
+
+    member = ""
+    for i, a in enumerate(clan.members, start=1):
+        member += f"`{i}.` {a.name} (th{a.town_hall})\n"
+
+    embed = discord.Embed(colour=discord.Colour.red(),
+                          title=f"Members of {clan.name}", description=member)
+    embed.set_thumbnail(url=clan.badge.url)
+    embed.set_footer(text=f"Total Members - {clan.member_count}/50")
+    await ctx.send(embed=embed)
+
+
+@bot.command()
+async def current_war_status(ctx, clan_tag):
+    if not utils.is_valid_tag(clan_tag):
+        await ctx.send("You didn't give me a proper tag!")
+        return
+
+    e = discord.Embed(colour=discord.Colour.blue())
+
+    try:
+        war = await ctx.bot.coc_client.get_current_war(clan_tag)
+    except coc.PrivateWarLog:
+        return await ctx.send("Clan has a private war log!")
+
+    if war is None:
+        return await ctx.send("Clan is in a strange CWL state!")
+
+    e.add_field(name="War State:", value=war.state, inline=False)
+
+    if war.end_time:  # if state is notInWar we will get errors
+
+        hours, remainder = divmod(int(war.end_time.seconds_until), 3600)
+        minutes, seconds = divmod(remainder, 60)
+
+        e.add_field(name=war.clan.name, value=war.clan.tag)
+        e.add_field(
+            name="Opponent:",
+            value=f"{war.opponent.name}\n" f"{war.opponent.tag}", inline=False)
+        e.add_field(name="War End Time:",
+                    value=f"{hours} hours {minutes} minutes {seconds} seconds",
+                    inline=False)
+
+    await ctx.send(embed=e)
+
+
+async def main():
+    logging.basicConfig(level=logging.ERROR)
+
+    async with coc.Client() as coc_client:
+        # Attempt to log into CoC API using your credentials.
+        try:
+            await coc_client.login(os.environ.get("DEV_SITE_EMAIL"),
+                                   os.environ.get("DEV_SITE_PASSWORD"))
+        except coc.InvalidCredentials as error:
+            exit(error)
+
+        # Add the client session to the bot
+        bot.coc_client = coc_client
+        await bot.start(os.environ.get("DISCORD_BOT_TOKEN"))
+
+
+if __name__ == "__main__":
+    try:
+        bot.run(DISCORD_TOKEN)
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        pass
---
+++ examples/war_logs.py
@@ -0,0 +1,139 @@
+import asyncio
+import os
+
+import coc
+from coc.raid import RaidLogEntry
+
+
+async def get_war_log_for_clans(client: coc.Client, clan_tags: list):
+    war_logs = {}
+    for tag in clan_tags:
+        # if we're not allowed to view war_log (private in game),
+        # this will raise an exception
+        try:
+            war_log = await client.get_war_log(tag)
+        except coc.PrivateWarLog:
+            war_log = []
+
+        war_logs[tag] = war_log
+
+    return war_logs
+
+
+async def test_raid_log(client: coc.Client, clan_tag: str):
+    # Limit is set to None retrieving all values
+    raid_no_page = await client.get_raid_log(clan_tag)
+    limit = len(raid_no_page)
+    page_limit = 30
+
+    # Enable pagination, by default it will only cache 10 logs using limit
+    # once you iterate beyond the cached amount, it will fetch the next set
+    raid_with_page = await client.get_raid_log(clan_tag, page=True, limit=page_limit)
+
+    # Iterate over war_logs like the current version of coc.py
+    for i, e in enumerate(raid_no_page):
+        e: RaidLogEntry
+        # print(f"[{i}]-sync limit: {limit} page: False {e.start_time.time}")
+
+    # Option to async for loop a non paginated object
+    count = 0
+    async for i in raid_no_page:
+        # print(f"[{count}]-async limit: {limit} page: False")
+        count += 1
+
+    # for i, e in enumerate(raid_with_page):
+    #     print(f"[{i}]-sync limit: {page_limit} page: True")
+
+    # Set `paginate=True` to enable fetching beyond the limit value until
+    # there are more values to fetch
+    count = 0
+    async for i in raid_with_page:
+        # print(f"[{count}]-async limit: {page_limit} page: True {i.start_time.time}")
+        count += 1
+
+    # Simple test comparing the two data sets
+    count = 0
+    async for async_log in raid_with_page:
+        if async_log != raid_no_page[count]:
+            raise AssertionError(f"{id(async_log)} does not match {id(raid_no_page[count])} at index {count}")
+        count += 1
+
+
+async def test_war_log(client: coc.Client, clan_tag: str):
+    # Limit is set to None retrieving all values
+    war_logs_no_page = await client.get_war_log(clan_tag)
+    limit = len(war_logs_no_page)
+    pagination_limit = 11
+
+    # Enable pagination, by default it will only cache 10 logs using limit
+    # once you iterate beyond the cached amount, it will fetch the next set
+    war_logs_with_page = await client.get_war_log(clan_tag, page=True, limit=pagination_limit)
+
+    # # Iterate over war_logs like the current version of coc.py
+    # for i, e in enumerate(war_logs_no_page):
+    #     print(f"[{i}]-sync limit: {limit} page: False")
+
+    # Option to async for loop a non paginated object
+    count = 0
+    async for i in war_logs_no_page:
+        # print(f"[{count}]-async limit: {limit} page: False")
+        count += 1
+
+    # for i, e in enumerate(war_logs_with_page):
+    #     print(f"[{i}]-sync limit: {pagination_limit} page: True")
+
+    # Set `paginate=True` to enable fetching beyond the limit value until
+    # there are more values to fetch
+    count = 0
+    async for i in war_logs_with_page:
+        # print(f"[{count}]-async limit: {pagination_limit} page: True {i.end_time.time}")
+        count += 1
+
+    # Simple test comparing the two data sets
+    count = 0
+    async for async_log in war_logs_with_page:
+        if async_log != war_logs_no_page[count]:
+            raise AssertionError(f"{id(async_log)} does not match {id(war_logs_no_page[count])} at index {count}")
+        count += 1
+    print(count)
+
+
+async def get_clan_tags_names(client: coc.Client, name: str, limit: int):
+    clans = await client.search_clans(name=name, limit=limit)
+    # return a list of tuples of name/tag pair ie.
+    # [(name, tag), (another name, another tag)]
+    return [(n.name, n.tag) for n in clans]
+
+
+async def get_war_log_opponents_from_clan_name(client: coc.Client, name: str, no_of_clans: int):
+    clan_tags_names = await get_clan_tags_names(client, name, no_of_clans)
+
+    # search for war logs with clan tags found
+    war_logs = await get_war_log_for_clans(client, [n[1] for n in clan_tags_names])
+
+    for name, tag in clan_tags_names:
+        # iterate over the wars
+        for war_log in war_logs[tag]:
+            # if it is a league war we will error below because it does not
+            # return a WarLog object, and thus no opponent
+            if war_log.is_league_entry:
+                print("League War Season - No opponent info available")
+            else:
+                print(f"War: {war_log.clan.name} vs {war_log.opponent.name}")
+
+
+async def main():
+    async with coc.Client() as coc_client:
+        try:
+            await coc_client.login(os.environ.get("DEV_SITE_EMAIL"),
+                                   os.environ.get("DEV_SITE_PASSWORD"))
+        except coc.InvalidCredentials as error:
+            exit(error)
+
+        await get_war_log_opponents_from_clan_name(coc_client, "Reddit Zulu", 5)
+        await test_war_log(coc_client, "#2Y28CGP8")
+        await test_raid_log(coc_client, "#2Y28CGP8")
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
