8aa2124e992e6e96e07685a8aceece1a11e9f11a
SeanDuffie <duffiesean@gmail.com>
2024-08-28 17:00:04 -0400

    Implemented basic Player object placeholder

---
+++ player.py
@@ -0,0 +1,11 @@
+""" @file player.py
+    @author Sean Duffie
+    @brief
+"""
+
+class Player:
+    def __init__(self, player_id: str):
+        self.id = player_id
+
+if __name__ == "__main__":
+    plyr = Player("#2QJ9QJ8R")
